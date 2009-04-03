0e1f4d2ecbb33cc48230e8307871b589410bc6b3
eruiz <morelies@gmail.com>
2009-04-03 10:10:31 -0700

    cobarder alias added!

--- chiquitonizer.sh
+++ chiquitonizer.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 
 echo "# Chiquitonizer aliases!" >> ~/.bashrc
+
 echo "alias sshr='ssh'" >> ~/.bashrc
+echo "alias cobarder='halt' >> ~/bashrc
+
+echo "# Chiquitonizer aliases end" >> ~/bashrc
 
 echo "Chiquitonizer running!!"
 
