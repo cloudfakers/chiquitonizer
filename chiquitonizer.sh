90501a312cb1d37b0857b63fc342067aa6a582b0
eruiz <morelies@gmail.com>
2009-04-03 10:11:10 -0700

    Fixed

--- chiquitonizer.sh
+++ chiquitonizer.sh
@@ -3,7 +3,7 @@
 echo "# Chiquitonizer aliases!" >> ~/.bashrc
 
 echo "alias sshr='ssh'" >> ~/.bashrc
-echo "alias cobarder='halt' >> ~/bashrc
+echo "alias cobarder='halt'" >> ~/bashrc
 
 echo "# Chiquitonizer aliases end" >> ~/bashrc
 
